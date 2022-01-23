9f9b0d8ec267e86c283d52ee6ccdc455b34c576b
Abdullah Al Zubaer <23630737+abdullahalzubaer@users.noreply.github.com>
2022-01-23 19:15:24 +0100

    Update corey_spider.py

--- corey/corey/spiders/corey_spider.py
+++ corey/corey/spiders/corey_spider.py
@@ -1,8 +1,6 @@
-import itertools
-
 import scrapy
 
-# For the first page (did by myself)
+# For the first page
 '''
 class CoreySpdier(scrapy.Spider):
     name = "corey"
@@ -18,8 +16,8 @@ class CoreySpdier(scrapy.Spider):
 '''
 
 
-# For all the page (did by myself)
-# '''
+# For all the page
+
 class CoreySpdier(scrapy.Spider):
     name = "corey"
     start_urls = ["https://coreyms.com/"]
@@ -35,34 +33,7 @@ class CoreySpdier(scrapy.Spider):
         if next_page is not None:
             next_page = response.urljoin(next_page)
             yield scrapy.Request(next_page, callback=self.parse)
-            # < li
 
-            # class ="pagination-next" > < a href="https://coreyms.com/page/2" > Next Page » < / a > < / li >
-
-
-# '''
-
-'''
-class QuotesSpider(scrapy.Spider):
-    name = "quotes"
-    start_urls = [
-        'http://quotes.toscrape.com/page/1/',
-    ]
-
-    def parse(self, response):
-        for quote in response.css('div.quote'):
-            yield {
-                'text': quote.css('span.text::text').get(),
-                'author': quote.css('small.author::text').get(),
-                'tags': quote.css('div.tags a.tag::text').getall(),
-            }
-
-        next_page = response.css('li.next a::attr(href)').get()
-        if next_page is not None:
-            next_page = response.urljoin(next_page)
-            yield scrapy.Request(next_page, callback=self.parse)
-            
-'''
 
 '''
 For corey MS homepage
@@ -79,19 +50,4 @@ main_post.css('time::text').get()
 
 main_post.css('span::text').get()
 >>> Will provide the author
-
-1. To ge the title (stupid way)
-Here i am accessing individual thing separately (no hierarchy)
-
-post_title = response.css("a.entry-title-link")[0]
-post_title.css('a::text').get()
->>> Will provide the title
-
-post_date = response.css("time.entry-time")[0]
-post_date.css('time::text').get()
->>> Will provide date of creation
-
-post_author = response.css("a.entry-author-link")[0]
-post_author.css('span::text').get()
->>> will provide the author
 '''
